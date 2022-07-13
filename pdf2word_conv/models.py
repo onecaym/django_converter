10140c1d4a16e233f804b5cf1c5e9c8a67f82a9e
onecaym <osipov99@list.ru>
2022-07-13 15:53:05 +0300

    Project is working with another script logic

--- pdf2word_conv/models.py
+++ pdf2word_conv/models.py
@@ -4,6 +4,7 @@ from django.core.files import File
 # Create your models here.
 
 class Uploaded_File(models.Model):
+	id = models.IntegerField(primary_key=True);
 	file = models.FileField(upload_to='')
 
 	def __str__(self):
